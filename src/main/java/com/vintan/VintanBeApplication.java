49d2c39796e1ac6fc03fca889a3e04fa267eedd5
hyeokkiyaa <khm38607574@gmail.com>
2025-08-14 14:03:57 +0900

    :

---
+++ src/main/java/com/vintan/VintanBeApplication.java
@@ -0,0 +1,13 @@
+package com.vintan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintanBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VintanBeApplication.class, args);
+    }
+
+}
