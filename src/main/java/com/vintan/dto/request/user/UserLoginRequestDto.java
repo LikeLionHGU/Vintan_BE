819bb97994092778b62db996c9a891d2dcce3c63
hyeokkiyaa <khm38607574@gmail.com>
2025-08-15 15:02:43 +0900

    feat: user login and session is hold

--- src/main/java/com/vintan/dto/request/user/UserLoginRequestDto.java
+++ src/main/java/com/vintan/dto/request/user/UserLoginRequestDto.java
@@ -1,4 +1,14 @@
 package com.vintan.dto.request.user;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserLoginRequestDto {
+    private String id;
+    private String password;
 }
