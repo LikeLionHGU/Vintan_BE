9c9aec718f7658293d6a8d782ce58068e0e0fa85
hyeokkiyaa <khm38607574@gmail.com>
2025-08-15 14:36:44 +0900

    add: register added

---
+++ src/main/java/com/vintan/dto/response/user/RegisterResponseDto.java
@@ -0,0 +1,10 @@
+package com.vintan.dto.response.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RegisterResponseDto {
+    private int isRegistered;
+}
